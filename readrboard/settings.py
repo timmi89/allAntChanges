e4c01d61c57dbde2488c014138f97df8ddf7acc8
erchaves <eric@readrboard.com>
2010-11-21 11:46:06 -0500

    clean folder ready - made file paths relative

--- readrboard/settings.py
+++ readrboard/settings.py
@@ -13,11 +13,13 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/usr/share/readrboard/readrdb.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'readrboard/readrdb.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+FILE_REL_PATH = '/usr/share/'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -72,7 +74,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/usr/share/readrboard/rb/templates"
+    "readrboard/rb/templates"
 )
 
 INSTALLED_APPS = (
