d9b006cd33c640d1cebd5834d204e0dc3b4c99b9
Tyler <tyler@readrboard.com>
2010-11-28 17:50:14 -0500

    pruned git

--- readrboard/rb/admin.py
+++ readrboard/rb/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:readrboard/rb/admin.py
 from rb.models import *
 from django.contrib import admin
 
@@ -7,16 +6,6 @@ admin.site.register(RBNode)
 admin.site.register(Tag)
 admin.site.register(Rating)
 admin.site.register(ReadrUser)
-=======
-from rb.models import Node
-from django.contrib import admin
 
-class NodeAdmin(admin.ModelAdmin):
-    list_display = ('user','parent','created','modified','body')
-    list_filter = ('user',)
-    date_hierarchy = 'modified'
-    search_fields = ('body',)
-    fields = ('user','parent','body')
 
-admin.site.register(Node, NodeAdmin)
->>>>>>> 85cf3579d8b7dcc7e8987d7dffb93cc008c98403:readrboard/rb/admin.py
+
--- readrboard/rb/models.py
+++ readrboard/rb/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< HEAD:readrboard/rb/models.py
 from django.contrib.sites.models import Site
 import datetime
 
@@ -41,28 +40,3 @@ class FacebookProfileModel(models.Model):
 
 class ReadrUser(FacebookProfileModel):
     user = models.ForeignKey(User, unique=True)
-=======
-import datetime
-
-# Create your models here.
-class Node(models.Model):
-    # Time fields
-    created = models.DateField(auto_now_add=True, editable=False, verbose_name='time-created')
-    modified = models.DateField(auto_now=True, editable=False, verbose_name='time-modified')
-    # Connectors
-    user = models.ForeignKey(User)
-    parent = models.ForeignKey('self', null=True, blank=True)
-    # Content
-    body = models.TextField()
-    shorturl = models.URLField(blank=True)
-    
-    def __unicode__(self):
-        title = self.body
-        if len(self.body) > 50:
-            title = u"%s ..." % title[0:50]
-        return title
-
-    class Meta:
-        verbose_name_plural = 'nodes'
-        ordering = ['-modified']
->>>>>>> 85cf3579d8b7dcc7e8987d7dffb93cc008c98403:readrboard/rb/models.py
