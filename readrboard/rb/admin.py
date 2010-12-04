88d226695fd552957444fd1c8ed6680a763708e2
erchaves <eric@readrboard.com>
2010-12-04 17:43:06 -0500

    fixed errors for now by commenting out imageField and admin site registers

--- readrboard/rb/admin.py
+++ readrboard/rb/admin.py
@@ -1,11 +1,12 @@
 from rb.models import *
 from django.contrib import admin
 
+#eric: these were causing an error - Comment out for now
+#todo: checkout error
+"""
 admin.site.register(Page)
 admin.site.register(RBNode)
 admin.site.register(Tag)
 admin.site.register(Group)
 admin.site.register(ReadrUser)
-
-
-
+"""
