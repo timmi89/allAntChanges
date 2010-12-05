9b9498a1b6f1b3eb411ed896e69d6bea57b18be7
Tyler <tyler@readrboard.com>
2010-12-05 11:43:28 -0500

    set up static pages

--- readrboard/urls.py
+++ readrboard/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -29,3 +30,8 @@ urlpatterns = patterns('',
     
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
