be43e82f29a74efd9eb0bdf4feae07a53f3ab1c0
porter <porter@readrboard.com>
2011-01-16 14:19:23 -0500

    lots of ui tweaks and enhancements

--- rb/media/ui-prototype/todo.js
+++ rb/media/ui-prototype/todo.js
@@ -1,15 +1,18 @@
 /*
 TODAY
 	make tooltip icons active based on activity
-	image interaction
-		small tooltip hover detection is odd
-		timing odd
-	
+
 	Rate - success view
 		show small window:  "also share on..."
 		"interesting, booooring, lol: <content>"
 		X to close
 		"will close in 4..3..2..1.."
+
+	Rate
+		if I have a rate window open, then open another, do both still work?
+
+	Chrome
+		occasionally draws the rindow too low?
 
 ---------------------------------------------
 
