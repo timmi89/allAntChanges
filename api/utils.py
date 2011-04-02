755880e15262ef7c1f5ab76ee295fa2e1974fd42
Tyler Brock <tyler.brock@gmail.com>
2011-04-02 12:56:10 -0400

    new

--- api/utils.py
+++ api/utils.py
@@ -1,7 +1,75 @@
 from readrboard.rb.models import *
 from django.utils.hashcompat import sha_constructor
-import datetime
+from datetime import datetime, timedelta
 import json
+
+def createSocialAuth(social_user, django_user, group_id, expiration):
+    # Create expiration time from Facebook timestamp
+    dt = datetime.fromtimestamp(expiration)
+
+    # Get the group secret which only we know
+    group_secret = Group.objects.get(id=group_id).secret
+
+    # Create the readr_token
+    readr_token = createToken(django_user.id, access_token, group_secret)
+
+    # Store the information and link it to the SocialUser
+    social_auth = SocialAuth.objects.get_or_create(
+        social_user = social_user,
+        auth_token = access_token,
+        expires = dt
+    )
+
+    # Remove stale tokens (if they exist)
+    SocialAuth.objects.all().filter(social_user=social_user).exclude(readr_token=readr_token).delete()
+
+def createSocialUser(django_user, profile):
+    # Create social user object for user
+    social = SocialUser.objects.get_or_create(
+        user = django_user,
+        provider = 'Facebook',
+        uid = profile['id'],
+        defaults = {
+            "full_name": profile['name'],
+            "username": profile.get('username', None),
+            "gender": profile.get('gender', None),
+            "hometown": profile['hometown']['name'] if (profile.get('hometown', None)) else None,
+            "bio": profile.get('bio', None)
+        }
+    )
+
+    # Print out the result
+    social_user = social[0]
+    result = ("Created new" if social[1] else "Retreived existing")
+    print result, "social user %s (%s: %s)" % (
+        social_user.full_name,
+        social_user.provider, 
+        social_user.uid
+    )
+
+    return social_user
+
+def createDjangoUser(profile):
+    # Create new Django user if one doesn't exist
+    user = User.objects.get_or_create(
+        username=profile['email'],
+        defaults = {
+            "email": profile['email'],
+            "first_name": profile['first_name'].capitalize(),
+            "last_name": profile['last_name'].capitalize(),
+        },
+    )
+
+    # Print out the result
+    django_user = user[0]
+    result = "Created new" if user[1] else "Retreived existing"
+    print result, "django user %s %s (%s)" % (
+        django_user.first_name, 
+        django_user.last_name, 
+        django_user.email
+    )
+
+    return django_user
 
 def checkToken(request):
     """
@@ -19,11 +87,11 @@ def createToken(djangoid, auth_token, group_secret):
     auth token and group secret
     """
     print "Creating readr_token"
-        return sha_constructor(
-            unicode(djangoid) +
-            unicode(auth_token) +
-            unicode(group_secret)
-        ).hexdigest()[::2]
+    return sha_constructor(
+        unicode(djangoid) +
+        unicode(auth_token) +
+        unicode(group_secret)
+    ).hexdigest()[::2]
 
 def getPage(request, pageid=None):
     canonical = request.GET.get('canonical_url', None)
@@ -51,7 +119,7 @@ def createInteractionNode(kind, body):
 def createInteraction(page, content, user, interaction_node, parent=None):
     if content and user and interaction_node:
         # Can't rely on Django's auto_now to create the time before storing the node
-        now = created=datetime.datetime.now()
+        now = created=datetime.now()
         if parent:
             print "Creating Interaction with parent node"
             new = parent.add_child(page=page, 
--- rb/models.py
+++ rb/models.py
@@ -173,6 +173,7 @@ class SocialUser(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
     hometown = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
+    image_url = models.URLField(blank=True, verify_exists=False)
 
     class Meta:
         unique_together = ('provider', 'uid')
