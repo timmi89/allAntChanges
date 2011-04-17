bab02b4a401a6b3ed2cf4d951b3776a15c648a78
Tyler Brock <tyler.brock@gmail.com>
2011-04-17 10:36:32 -0400

    enabled django debug toolbar

--- settings.py
+++ settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'readrboard.urls'
@@ -94,6 +95,7 @@ INSTALLED_APPS = (
     'piston',
     'south',
     'treebeard',
+    'debug_toolbar',
 )
 
 # for sessions
@@ -102,6 +104,21 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 # for lazy signup
 AUTHENTICATION_BACKENDS = (
   'django.contrib.auth.backends.ModelBackend',
+)
+
+# For django debug toolbar
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 # for social auth
