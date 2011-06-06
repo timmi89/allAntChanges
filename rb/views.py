8c73276b36c07c23a4f38c771bd0c7140a5fbded
Tyler Brock <tyler.brock@gmail.com>
2011-06-05 23:07:03 -0400

    temp look here for cards

--- rb/views.py
+++ rb/views.py
@@ -85,9 +85,9 @@ def makeCard(page, interactions):
 
 def cards(request):
     pages = Page.objects.all().annotate(interaction_count=Count('interaction'))[:5]
-    interactions = Interaction.objects.filter(page__in=pages)
-    cards = [makeCard(page, interactions) for page in pages]
-    context = {'cards': cards}
+    interactions = Interaction.objects.filter(page__in=pages).order_by('created')
+    #cards = [makeCard(page, interactions) for page in pages]
+    context = {'interactions': interactions, 'cards': cards}
     return render_to_response("cards.html", context)
 
 def sidebar(request):
