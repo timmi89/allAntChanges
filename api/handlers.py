6e75af9358083848813e4e220eddcf5916fbaa05
Tyler Brock <tyler.brock@gmail.com>
2011-06-18 12:44:08 -0400

    fix

--- api/handlers.py
+++ api/handlers.py
@@ -116,13 +116,16 @@ class TagHandler(InteractionHandler):
 
         new_interaction = None
 
-        # Get or create InteractionNode for tag
-        if tag_id:
-            # ID known retrieve existing
-            inode = InteractionNode.objects.get(id=tag_id)
-        elif tag_body:
-            # No id provided, using body to get_or_create
-            inode = InteractionNode.objects.get_or_create(body=tag_body)
+        # Get or create InteractionNode for share
+        try:
+            if tag_id:
+                # ID known retrieve existing
+                inode = InteractionNode.objects.get(id=tag_id)
+            elif tag_body:
+                # No id provided, using body to get_or_create
+                inode = InteractionNode.objects.get_or_create(body=tag_body)[0]
+        except:
+            raise JSONException(u'Error creating or retrieving interaction node')
 
         
         new_interaction = createInteraction(page, container, content, user, 'tag', inode, group)
@@ -152,7 +155,7 @@ class ShareHandler(InteractionHandler):
                 inode = InteractionNode.objects.get(id=tag_id)
             elif tag_body:
                 # No id provided, using body to get_or_create
-                inode = InteractionNode.objects.get_or_create(body=tag_body)
+                inode = InteractionNode.objects.get_or_create(body=tag_body)[0]
         except:
             raise JSONException(u'Error creating or retrieving interaction node')
 
