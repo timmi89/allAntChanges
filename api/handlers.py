0ec8e026d05113ce11d4c6aad703ae49d9447e66
Tyler Brock <tyler.brock@gmail.com>
2011-07-03 17:19:25 -0400

    group changes

--- api/handlers.py
+++ api/handlers.py
@@ -335,6 +335,6 @@ class SettingsHandler(AnonymousBaseHandler):
             if host in domains:
                 return group_object
             else:
-                raise JSONException("Group (" + str(group) + ") settings request invalid for this domain (" + host + ")")
+                raise JSONException("Group (" + str(group) + ") settings request invalid for this domain (" + host + ")" + domains)
         else:
             return ("Group not specified")
