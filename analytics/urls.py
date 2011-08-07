80775e0eac1153f9cbc05a8d23c5012c6bf1d073
Tyler Brock <tyler.brock@gmail.com>
2011-08-07 19:04:24 -0400

    analytics update

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'group/^(?P<short_name>[\w\-]+)/$', 'analytics.views.analytics'),
+    url(r'^group/(?P<short_name>[\w\-]+)/$', 'analytics.views.analytics'),
+    url(r'^group/(?P<short_name>[\w\-]+)/tags/popular/$', 'analytics.views.popular', kwargs={"kind":"tag"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/tags/frequency/$', 'analytics.views.frequency', kwargs={"kind":"tag"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/bookmarks/popular$', 'analytics.views.popular', kwargs={"kind":"bkm"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/shares/popular/$', 'analytics.views.popular', kwargs={"kind":"shr"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/content/active/$', 'analytics.views.active', kwargs={"subject":"content"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/users/active/$', 'analytics.views.active', kwargs={"subject":"users"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/pages/active/$', 'analytics.views.active', kwargs={"subject":"pages"}),
+    url(r'^group/(?P<short_name>[\w\-]+)/pages/recent/$', 'analytics.views.recent'),
+    url(r'^group/(?P<short_name>[\w\-]+)/pages/tagged/$', 'analytics.views.tagged'),
 )
--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,58 @@
 from rb.models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
+from datetime import datetime
+from django.db.models import Count
+from django.core import serializers
 
 def analytics(request, short_name=None):
-    return HttpResponse("Test")
+    group = Group.objects.get(short_name=short_name)
+    context = {}
+    context['group'] = group
+    return render_to_response("analytics.html", context)
+
+def analytics_request(func):
+    def wrapper(request, *args, **kwargs):
+        params = request.GET
+        data = {}
+        data['start'] = params.get('start', None)
+        data['end'] = params.get('end', None)
+        data['max_count'] = params.get('max_count', None)
+        return func(request, data, *args, **kwargs)
+    return wrapper
+    
+@analytics_request
+def popular(request, data, short_name, **kwargs):
+    interactions = Interaction.objects.all()
+    group = Group.objects.get(short_name=short_name)
+    interactions = interactions.filter(page__site__group=group)
+    interacitons = interactions.filter(kind=kwargs['kind'])
+    
+    if data['start']:
+        interactions = interactions.filter(created__gte=datetime.strptime(data['start'], "%m/%d/%y"))
+    if data['end']:
+        interactions = interactions.filter(created__lte=datetime.strptime(data['end'], "%m/%d/%y"))
+    if data['max_count']:
+        interactions = interactions[:max_count]
+    
+    interactions = interactions.order_by('interaction_node').values('interaction_node')
+    interactions = interactions.annotate(count=Count('id'))
+    
+    return HttpResponse(interactions)
+    
+    
+@analytics_request
+def active(request, short_name):
+    pass
+
+@analytics_request
+def recent(request, short_name):
+    pass
+
+@analytics_request
+def tagged(request, short_name):
+    pass
+
+@analytics_request
+def frequency(request, short_name):
+    pass
