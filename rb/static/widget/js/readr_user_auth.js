14367f2b7504232c3a9178ae5da0e6ffd45933d6
porterbayne <porter@readrboard.com>
2011-09-10 00:42:22 -0400

    site works but currently requires a call to FB on every page load, we need to redo some of this.

--- rb/static/widget/js/readr_user_auth.js
+++ rb/static/widget/js/readr_user_auth.js
@@ -69,9 +69,10 @@ RDRAuth = {
 		}
 	},
 	getReadrToken: function(fb_response, callback ) {
-
+console.log('RDRAuth.getReadrToken');
 		if ( fb_response ) {
-            var fb_session = (fb_response.session) ? fb_response.session:fb_response
+			console.log('RDRAuth.getReadrToken 1');
+            var fb_session = (fb_response.authResponse) ? fb_response.authResponse:fb_response
 			var sendData = {
 				fb: fb_session,
 				group_id: (qs_args.group_id) ? qs_args.group_id:1, // TODO aaaaaaaaaaaaaaagh remove GROUP ID and replace with NONCE
@@ -101,6 +102,7 @@ RDRAuth = {
 				}
 			});
 		} else {
+			console.log('RDRAuth.getReadrToken 2');
 			RDRAuth.doFBLogin();
 		}
 	},
@@ -152,19 +154,25 @@ RDRAuth = {
 		}
 	},
 	reauthUser : function(args) {
+		console.log('RDRAuth.reauthUser');
 		RDRAuth.readUserCookie();
 		if ( !FB.getAuthResponse() ) {
+			console.log('RDRAuth.reauthUser 1');
 			FB.getLoginStatus(function(response) {
-		  		if (response && response.session) {
-					// TODO:  suspect we only need to killUser if there is a FB session change.
+		  		if (response && response.status == "connected") {
+		  			console.log('RDRAuth.reauthUser 1a');
 					RDRAuth.killUser( function(response) {
+						console.log('KillUser CALLBACK');
+						console.dir(response);
 						RDRAuth.getReadrToken(response); // function exists in readr_user_auth.js
-					});
+					}, response);
 		  		} else {
+		  			console.log('RDRAuth.reauthUser 1b');
 		  			RDRAuth.notifyParent("", "fb user needs to login");
 		  		}
 		  	});
 		} else {
+			console.log('RDRAuth.reauthUser 2');
 			RDRAuth.killUser( function(response) {
 				RDRAuth.getReadrToken(response); // function exists in readr_user_auth.js
 			});
@@ -172,82 +180,54 @@ RDRAuth = {
 		}
 	},
 	checkFBStatus : function(args) {
-		FB.getLoginStatus(function(response) {
-			if ( response.authResponse && response.status && response.status == "connected" ) {
-				switch (args.requesting_action) {
-					case "admin_request":
-						// this call is from the website
-						// still using this?
-						$('#fb-logged-in').show();
-						$('#fb-logged-in button').click( function() {
-							if ( RB ) RB.admin.requestAccess( response, args.group_id );
-						});
-						$('#fb-logged-out').hide();
-						break;
-					
-					case "site_login":
-						RDRAuth.getReadrToken( response.authResponse, function() { window.location.reload(); });
-						break;
-
-					case "widget_login":
-						$('#fb-logged-in').show().css('visibility','visible');
-						$('#fb-logged-out').hide().css('visibility','hidden'); 
-						break;
-
-					case "site_load":
+console.log('checkFBStatus');
+		FB.getLoginStatus( function(response) {
+			console.log('checkFBStatus 1');
+			console.dir(response);
+			if (response.status && response.status == "connected" ) {
+				if (top == self) {
+					console.log('checkFBStatus 1a');
+					// QUICK KLUDGE
+					// TODO what we really need is a /api/getUserInfo call to get first/last name from our server, and store it in a session, not a cookie
+					// now write the html for the user
+					if ( $.cookie('user_id') || ( RDRAuth.rdr_user && RDRAuth.rdr_user.user_id ) ) {
+						var user_id = ($.cookie('user_id')) ? $.cookie('user_id'):RDRAuth.rdr_user.user_id;
+						var img_url = ($.cookie('img_url')) ? $.cookie('img_url'):RDRAuth.rdr_user.img_url;
 
 						$('#fb-logged-in').show().css('visibility','visible');
 						$('#fb-logged-out').hide().css('visibility','hidden');
-						
+						FB.api('/me', function(response) {
+		      				var $user = $('<a/>'),
+							$avatar = $('<img/>'),
+							$name = $('<strong/>');
 
-						// QUICK KLUDGE
-						// TODO what we really need is a /api/getUserInfo call to get first/last name from our server, and store it in a session, not a cookie
-						// now write the html for the user
-						if ( $.cookie('user_id') && $.cookie('user_id') ) {
-							FB.api('/me', function(response) {
-			      				var $user = $('<a/>'),
-								$avatar = $('<img/>'),
-								$name = $('<strong/>');
+							$user.attr('href', '/user/'+user_id );
+							$avatar.attr('src', img_url + '?type=square');
+							$name.text( response.name );
 
-								$user.attr('href', '/user/'+$.cookie('user_id') );
-								$avatar.attr('src', $.cookie('img_url') + '?type=square');
-								$name.text( response.name );
+							$user.append( $avatar, $name );
 
-								$user.append( $avatar, $name );
-
-								$('#fb-logged-in').html( $user );
-			      			});
-						} else {
-							RDRAuth.getReadrToken( response.authResponse, function() { window.location.reload(); });
-						}
-						break;
-
+							$('#fb-logged-in').html( $user );
+		      			});
+					} else {
+						console.log('checkFBStatus 1b');
+						RDRAuth.getReadrToken( response.authResponse, function() { console.log('window.location.reload(); 2'); });
+					}
 				}
 			} else {
-				switch (args.requesting_action) {
-					case "admin_request":
-						// this call is from the website
-						// still using this?
-						$('#fb-logged-in').hide().css('visibility','hidden');
-						$('#fb-logged-out').show().css('visibility','visible');
-						break;
-
-					case "site_login":
-						// $('#fb-logged-in').hide();
-						// $('#fb-logged-out').show();
-						// RDRAuth.getReadrToken( response, function() { window.location.reload(); });
-						break;
-
-					case "site_load":
-						$('#fb-logged-in').hide().css('visibility','hidden');
-						$('#fb-logged-out').show().css('visibility','visible');
-						// RDRAuth.getReadrToken( response, true );
-						break;
-				}
+				console.log('checkFBStatus 2');
+				if (top == self) {
+					console.log('checkFBStatus 2a');
+					$('#fb-logged-in').hide().css('visibility','hidden');
+					$('#fb-logged-out').show().css('visibility','visible');
+					// RDRAuth.getReadrToken( response, true );
+				}	
 			}
 		});
 	},
 	setUser : function(response) {
+		console.log('RDRAuth.setUser');
+		console.dir(response);
 		RDRAuth.rdr_user = {};
 		// if no first_name attribute is in the response, this is a temporary user.
 		if ( response.data.first_name ) RDRAuth.rdr_user.temp_user = false;
@@ -262,33 +242,44 @@ RDRAuth = {
 		$.cookie('img_url', RDRAuth.rdr_user.img_url, { expires: 365, path: '/' });
 		$.cookie('user_id', RDRAuth.rdr_user.user_id, { expires: 365, path: '/' });
 		$.cookie('readr_token', RDRAuth.rdr_user.readr_token, { expires: 365, path: '/' });
+		$.cookie('rdr_session', 'true');
 	},
 	readUserCookie : function() {
-		RDRAuth.rdr_user.first_name = $.cookie('first_name');
-		RDRAuth.rdr_user.full_name = $.cookie('full_name');
+		console.log('RDRAuth.readUserCookie');
+		// RDRAuth.rdr_user.first_name = $.cookie('first_name');
+		// RDRAuth.rdr_user.full_name = $.cookie('full_name');
 		if ( $.cookie('img_url') ) RDRAuth.rdr_user.img_url = $.cookie('img_url');
 		if ( $.cookie('user_id') ) RDRAuth.rdr_user.user_id = $.cookie('user_id');
 		if ( $.cookie('readr_token') ) RDRAuth.rdr_user.readr_token = $.cookie('readr_token');
 		if ( $.cookie('temp_user') ) RDRAuth.rdr_user.temp_user = $.cookie('temp_user');
 	},
 	returnUser : function() {
+		console.log('RDRAuth.returnUser');
 		RDRAuth.readUserCookie();
-		var sendData = {
-			// arguments are nested under data for consistency with passing values up to the parent
-			data : {
-				// first_name : RDRAuth.rdr_user.first_name,
-				// full_name : RDRAuth.rdr_user.full_name,
-				img_url : RDRAuth.rdr_user.img_url,
-				user_id : RDRAuth.rdr_user.user_id,
-				readr_token : RDRAuth.rdr_user.readr_token
-			}
-		};
-		RDRAuth.notifyParent(sendData, "returning_user");
+		if (top == self) {
+			// we're on the site
+			RDRAuth.checkFBStatus();
+		} else {
+			var sendData = {
+				// arguments are nested under data for consistency with passing values up to the parent
+				data : {
+					// first_name : RDRAuth.rdr_user.first_name,
+					// full_name : RDRAuth.rdr_user.full_name,
+					img_url : RDRAuth.rdr_user.img_url,
+					user_id : RDRAuth.rdr_user.user_id,
+					readr_token : RDRAuth.rdr_user.readr_token
+				}
+			};
+			RDRAuth.notifyParent(sendData, "returning_user");
+		}
 	},
-	killUser : function(callback) {
-
+	killUser : function(callback, callback_args) {
+console.log('RDRAuth.killUser');
+console.dir(callback_args);
+console.dir(RDRAuth.rdr_user);
 		// if ( RDRAuth.rdr_user && RDRAuth.rdr_user.user_id && RDRAuth.rdr_user.readr_token && RDRAuth.rdr_user.first_name ) {
-		if ( RDRAuth.rdr_user && RDRAuth.rdr_user.first_name ) {
+		if ( RDRAuth.rdr_user && RDRAuth.rdr_user.temp_user == "false" ) {
+			console.log('RDRAuth.killUser 1');
 			// deauth a full user
 			var sendData = {
 				user_id : RDRAuth.rdr_user.user_id,
@@ -299,61 +290,56 @@ RDRAuth = {
 				url: "/api/deauthorize/",
 				type: "get",
 				contentType: "application/json",
+				context: {callback_args:callback_args},
 				dataType: "jsonp",
 				data: {
 					json: JSON.stringify( sendData )
 				},
 				success: function(response){
-					// $.cookie('first_name', null, { path: '/' });
-					// $.cookie('full_name', null, { path: '/' });
+					console.dir(this.callback_args);
+					console.dir(callback_args);
+					$.cookie('first_name', null, { path: '/' });
+					$.cookie('full_name', null, { path: '/' });
 					$.cookie('img_url', null, { path: '/' });
 					$.cookie('user_id', null, { path: '/' });
 					$.cookie('readr_token', null, { path: '/' });
+					$.cookie('rdr_session', null);
 					RDRAuth.rdr_user = {};
-					if (callback) callback();
+					if (callback && this.callback_args) {
+						console.log('killUser SUCCESS 1');
+						callback(this.callback_args);
+					} else if (callback) {
+						console.log('killUser SUCCESS 2'); 
+						callback();
+					}
 				}
 			});
 		} else {
+			console.log('RDRAuth.killUser 2');
 			// just a temp user
 			$.cookie('img_url', null, { path: '/' });
 			$.cookie('user_id', null, { path: '/' });
 			$.cookie('readr_token', null, { path: '/' });
+			$.cookie('rdr_session', null);
+			$.cookie('temp_user', null);
 			RDRAuth.rdr_user = {};
-			if (callback) callback();
+			if (callback && callback_args) {
+				console.log('RDRAuth.killUser 2a');
+				callback(callback_args);
+			} else if (callback) {
+				console.log('RDRAuth.killUser 2b');
+				callback();
+			}
 		}
 	},
 	doFBLogin: function(requesting_action) {
 		// RDRAuth.doFBLogin
-
+console.log('RDRAuth.doFBLogin');
 		FB.login(function(response) {
 		  if (response.authResponse) {
-		    FB.api('/me', function(response) {
-
+		    // FB.api('/me', function(response) {
 		      RDRAuth.getReadrToken( FB.getAuthResponse(), function() {
-		      	RDRAuth.checkFBStatus( { user:user, requesting_action:requesting_action } );
-		      });
-
-
-		      /*
-		      jQuery1606043299664238055_1315254810872({
-			    "status": "success", 
-			    "data": {
-			        "user_id": 43, 
-			        "first_name": "Porter", 
-			        "img_url": "http://graph.facebook.com/613765056/picture", 
-			        "full_name": "Porter Bayne", 
-			        "readr_token": "0e3a4209a04003e3fa6e"
-			    }
-			})
-		      */
-
-
-
-
-
-
-		      // FB.logout(function(response) {
-
+		      	RDRAuth.checkFBStatus();
 		      // });
 		    });
 		  } else {
@@ -380,6 +366,7 @@ RDRAuth = {
 		});
 	},
 	init : function() {
+		console.log('RDRAuth.init();');
 		RDRAuth.returnUser();
 	},
 	decodeDjangoCookie : function(value) {
