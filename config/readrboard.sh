11744c009f771db69af3d33b7e6cee4fbe1a35cd
Tyler Brock <tbrock@debian.(none)>
2012-04-02 00:28:47 -0400

    settings changes that seemed to fix the server

--- config/readrboard.sh
+++ config/readrboard.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This is for Green Unicorn
 set -e
-NUM_WORKERS=2
-LOGFILE=/home/readrboard/readrboard/logs/gunicorn.log
+NUM_WORKERS=3
+LOGFILE=/home/tbrock/readrboard/logs/gunicorn.log
 
 # user/group to run as
-USER=readrboard
-GROUP=readrboard
-cd /home/readrboard/readrboard/
+USER=tbrock
+GROUP=tbrock
+cd /home/tbrock/readrboard/
 exec /usr/local/bin/gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
--- settings.py
+++ settings.py
@@ -203,9 +203,9 @@ if DEBUG: INSTALLED_APPS.append('devserver')
 if DEBUG: INSTALLED_APPS.append('django_extensions')
 
 DEVSERVER_MODULES = (
-    'devserver.modules.sql.SQLRealTimeModule',
-    'devserver.modules.sql.SQLSummaryModule',
-    'devserver.modules.profile.ProfileSummaryModule',
+    #'devserver.modules.sql.SQLRealTimeModule',
+    #'devserver.modules.sql.SQLSummaryModule',
+    #'devserver.modules.profile.ProfileSummaryModule',
 
     # Modules not enabled by default
     #'devserver.modules.ajax.AjaxDumpModule',
@@ -251,15 +251,15 @@ LOGGING = {
 }
 
 DEVSERVER_MODULES = (
-    'devserver.modules.sql.SQLRealTimeModule',
-    'devserver.modules.sql.SQLSummaryModule',
-    'devserver.modules.profile.ProfileSummaryModule',
+    #'devserver.modules.sql.SQLRealTimeModule',
+    #'devserver.modules.sql.SQLSummaryModule',
+    #'devserver.modules.profile.ProfileSummaryModule',
 
     # Modules not enabled by default
-    'devserver.modules.ajax.AjaxDumpModule',
-    'devserver.modules.profile.MemoryUseModule',
-    'devserver.modules.cache.CacheSummaryModule',
-    'devserver.modules.profile.LineProfilerModule',
+    #'devserver.modules.ajax.AjaxDumpModule',
+    #'devserver.modules.profile.MemoryUseModule',
+    #'devserver.modules.cache.CacheSummaryModule',
+    #'devserver.modules.profile.LineProfilerModule',
 )
 
 DEVSERVER_IGNORED_PREFIXES = ['/media', '/uploads']
