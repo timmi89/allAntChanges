adcff6f1589593f9502b1366975c58d673b947cf
porterbayne <porter@readrboard.com>
2012-04-08 15:10:31 -0400

    reenabling sql logging on local dev

--- settings.py
+++ settings.py
@@ -205,9 +205,9 @@ if DEBUG: INSTALLED_APPS.append('devserver')
 if DEBUG: INSTALLED_APPS.append('django_extensions')
 
 DEVSERVER_MODULES = (
-    #'devserver.modules.sql.SQLRealTimeModule',
-    #'devserver.modules.sql.SQLSummaryModule',
-    #'devserver.modules.profile.ProfileSummaryModule',
+    'devserver.modules.sql.SQLRealTimeModule',
+    'devserver.modules.sql.SQLSummaryModule',
+    'devserver.modules.profile.ProfileSummaryModule',
 
     # Modules not enabled by default
     #'devserver.modules.ajax.AjaxDumpModule',
