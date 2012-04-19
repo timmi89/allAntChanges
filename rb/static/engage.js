9f17a3a8fa875f01ad3705e4a4ea3382c777237a
porterbayne <porter@readrboard.com>
2012-04-19 19:20:28 -0400

    ok, giving in to drop shadows on stream for now.  thoughts?

--- rb/static/engage.js
+++ rb/static/engage.js
@@ -2,7 +2,8 @@ var RDR = {}, //our global RDR object
 $RDR, //our global $RDR object (jquerified RDR object for attaching data and queues and such)
 $R = {}, //init var: our clone of jQuery
 RDR_scriptPaths = {},
-RDR_offline = ( window.location.href.indexOf('local.readrboard.com') != -1 ) ? true:false,
+// RDR_offline = ( window.location.href.indexOf('local.readrboard.com') != -1 ) ? true:false,
+RDR_offline = true,
 RDR_baseUrl = ( RDR_offline ) ? "http://local.readrboard.com:8080":"http://www.readrboard.com",
 RDR_staticUrl = ( RDR_offline ) ? "http://local.readrboard.com:8080/static/":"http://s3.amazonaws.com/readrboard/",
 RDR_widgetCssStaticUrl = ( RDR_offline ) ? "http://local.readrboard.com:8080/static/":"http://s3.amazonaws.com/readrboard/";
