13808436f636d9c49b69848ce67112ce171f233a
porterbayne <porter@readrboard.com>
2012-04-23 09:28:25 -0400

    minor css update to make tag count a bit darker in summary box

--- rb/static/engage.js
+++ rb/static/engage.js
@@ -5518,7 +5518,7 @@ function $RFunctions($R){
         css.push( RDR_staticUrl+"widget/css/ie"+parseInt( $R.browser.version, 10) +".css" );
     }
 
-    css.push( RDR_widgetCssStaticUrl+"widget/css/widget.css?v1" );
+    css.push( RDR_widgetCssStaticUrl+"widget/css/widget.css?rv1" );
     css.push( RDR_scriptPaths.jqueryUI_CSS );
     css.push( RDR_staticUrl+"widget/css/jquery.jscrollpane.css" );
 
