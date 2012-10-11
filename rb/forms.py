fdca4e711a240e82a15f2c39e6af4d1fbaf11b96
Eric Chaves <erchaves@gmail.com>
2012-10-10 23:19:21 -0400

    keeping these commented out while i test

--- rb/forms.py
+++ rb/forms.py
@@ -281,9 +281,10 @@ class CreateGroupForm(forms.Form):
 
         msg = EmailMessage("ReadrBoard group admin approval", ga_approval_mail, "groups@readrboard.com", 
                                    [
-                                   'porterbayne@gmail.com',
+                                   #temporarily keeping these commented out
+                                   # 'porterbayne@gmail.com',
                                    'erchaves@gmail.com',
-                                   'michael@readrboard.com',
+                                   # 'michael@readrboard.com',
                                    ]
                             )
         msg.content_subtype='html'
