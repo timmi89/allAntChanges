6b9a5e4d3b35627a531083b1a020f78d53fda6c3
Michael Shaw <michael@abastionofsanity.com>
2012-12-09 20:06:45 -0500

    small worker process tweak

--- config/readrboard.sh
+++ config/readrboard.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This is for Green Unicorn
 set -e
-NUM_WORKERS=5
+NUM_WORKERS=3
 LOGFILE=/home/tbrock/readrboard/logs/gunicorn.log
 
 # user/group to run as
 USER=tbrock
 GROUP=tbrock
 cd /home/tbrock/readrboard/
-exec /usr/local/bin/gunicorn_django -w $NUM_WORKERS --max-requests=1000 --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
+exec /usr/local/bin/gunicorn_django -w $NUM_WORKERS --max-requests=500 --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
