e00a0900f2636875e01599219e6061154d4a60ba
root <root@debian.(none)>
2013-04-21 14:48:13 -0400

    longer memcache timeout

--- settings.py
+++ settings.py
@@ -160,7 +160,7 @@ else:
         'default': {
             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
             'LOCATION': '50.116.59.190:11211',
-            'TIMEOUT':600
+            'TIMEOUT':24000
         }
     }
 
