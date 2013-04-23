4c0b16768a3c075dfbda281afd9c5811554b0f19
porterbayne <porter@readrboard.com>
2013-04-23 01:10:17 -0400

    faster.  tagbox grid is wonky tho

--- rb/templatetags/custom_filters.py
+++ rb/templatetags/custom_filters.py
@@ -13,16 +13,16 @@ def get_interaction_count(interaction_node, page=None, content=None):
 def split_reaction(tagBodyRaw):
 	tagBodyRaw = tagBodyRaw
 	tagBody = ""
-	tagIsSplitClass = ""
+	tagBodyClass = "nospace"
 
 	if len(tagBodyRaw) > 16:
-		# do stuff
-		tagIsSplitClass = "rdr_tag_split"
 		if not ' ' in tagBodyRaw:
-			tagBody = '<div>' + tagBodyRaw[:15] + '-</div><div>' + tagBodyRaw[15:] + '</div>'
+			tagBody = '<div class="nospace charCount_'+str(len(tagBodyRaw[:15]))+'">' + tagBodyRaw[:15] + '-</div><div class="charCount_'+str(len(tagBodyRaw[15:]))+'">' + tagBodyRaw[15:] + '</div>'
 		else:
 			tagBody1 = ""
+			tagBody1Class = "nospace"
 			tagBody2 = ""
+			tagBody2Class = "nospace"
 			keepLooping = True
 
 			tagBodyRawSplit = collections.deque( tagBodyRaw.split(' ') )
@@ -31,13 +31,22 @@ def split_reaction(tagBodyRaw):
 				tagBody1 += tagBodyRawSplit.popleft() + ' '
 				if (len(tagBody1)+len(tagBodyRawSplit[0]) >= 16) or len(tagBodyRawSplit) == 0:
 					keepLooping = False
+			if ' ' in tagBody1.strip():
+				tagBody1Class = "space"
 
 			while len(tagBodyRawSplit) > 0:
 				tagBody2 += tagBodyRawSplit.popleft() + ' '
 			
-			tagBody = '<div>' + tagBody1 +'</div><div>' + tagBody2 + '</div>'
+			if ' ' in tagBody2.strip():
+				tagBody2Class = "space"
+			tagBody = '<div class="'+tagBody1Class+' charCount_'+str(len(tagBody1))+'">' + tagBody1 +'</div><div class="'+tagBody2Class+' charCount_'+str(len(tagBody2))+'">' + tagBody2 + '</div>'
 
 	else:
-		tagBody = '<div>' + tagBodyRaw + '</div>'
+		if ' ' in tagBody.strip():
+			tagBodyClass = "space"
+		tagBody = '<div class="'+tagBodyClass+' charCount_'+str(len(tagBodyRaw))+'">' + tagBodyRaw + '</div>'
 
 	return mark_safe(tagBody)
+
+
+
