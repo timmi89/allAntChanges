9d1f6b46f140153a914a55bc278a24717e094792
Porter Bayne <porter@readrboard.com>
2014-03-04 14:40:33 -0500

    css cachebust

--- rb/static/engage_full.js
+++ rb/static/engage_full.js
@@ -8927,7 +8927,7 @@ function $RFunctions($R){
         css.push( RDR_staticUrl+"widget/css/ie"+parseInt( $R.browser.version, 10) +".css" );
     }
 
-    css.push( RDR_widgetCssStaticUrl+"widget/css/widget.css?rv20" );
+    css.push( RDR_widgetCssStaticUrl+"widget/css/widget.css?rv21" );
     // css.push( RDR_scriptPaths.jqueryUI_CSS );
     css.push( RDR_staticUrl+"widget/css/jquery.jscrollpane.css" );
 
