2b6902a474c1b1465a0563850acaf3dfb1a033eb
Porter Bayne <porter@readrboard.com>
2014-07-27 16:58:00 -0400

    tracker, not events...

--- rb/static/widget/js/readr_user.js
+++ rb/static/widget/js/readr_user.js
@@ -19,8 +19,11 @@ function getWindowProps(options){
     return 'menubar=1,resizable=1,scrollbars=yes,width='+w+',height='+h+',top='+t+',left='+l;
 };
 
-var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://tracker.readrboard.com/insert"
-// var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://localnode.com:3000/insert"
+// var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://96.126.123.57/insert"
+// var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://69.164.203.230/insert"
+// var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://tracker.readrboard.com/insert"
+// var trackingUrl = (document.domain != "local.readrboard.com") ? "http://events.readrboard.com/insert" : "http://events.readrboard.com/insert"
+var trackingUrl = (document.domain != "local.readrboard.com") ? "http://tracker.readrboard.com/insert" : "http://localnode.com:3000/insert"
 
 // var trackingUrl = "http://events.readrboard.com/insert";
 
