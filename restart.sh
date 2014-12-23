ce6d135171f2cd3649a84f3702e9f506a4db2aef
Tech Team <tech@antenna.is>
2014-12-23 15:51:31 +0000

    restart script with dbs

---
+++ restart.sh
@@ -0,0 +1,10 @@
+echo "Setting up ssh-agent."
+ssh-add
+
+echo "Configuring environment."
+. deploytasks.sh
+
+foreach_exec_root "$DBS" "Restarting DATABASES" "$DBRESTART"
+
+foreach_exec_broadcaster "$ARRAY" "Basic update" "$BASIC"
+foreach_exec_root "$ARRAY" "Restart antenna" "$SUPER"
