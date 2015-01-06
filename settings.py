4db63c8e204f6c208c9a3c996393211408f99bc6
Tech Team <tech@antenna.is>
2015-01-06 13:32:42 +0000

    cache settings change for new server and off db slave

--- settings.py
+++ settings.py
@@ -186,7 +186,7 @@ else:
     CACHES = {
         'default': {
             'BACKEND': 'memcachepool.cache.UMemcacheCache',
-            'LOCATION': '192.168.171.12:11211',
+            'LOCATION': ['192.168.182.48:11211','192.168.182.177:11211'],
             'TIMEOUT':86400,
             'OPTIONS': {
                 'MAX_POOL_SIZE': 100,
