ab241058367ef516507cf12aea78fecc37c42d6b
Michael Shaw <michaelshaw23@gmail.com>
2015-01-07 02:30:45 -0500

    update for new servers

--- deploytasks.sh
+++ deploytasks.sh
@@ -1,7 +1,7 @@
-ARRAY="array1 array2 array3 array4 array5 array6 array7 array8 array9 array10"
+ARRAY="array1 array2 array3 array4 array5 array6 array7 array8 array9 array10 array11 array12 array13 array14"
 UTIL="util1"
 DBS="rbprod rbslave1 dbslave2"
-
+EVENTS="events2 events3 events4 events5 events6 events7 events8 events9"
 
 BASIC='source .profile; ssh-add .ssh/id_rsa_git; cd antenna; git pull origin master;'
 FULL='source .profile; ssh-add .ssh/id_rsa_git; cd antenna; git pull origin master; ./manage.py collectstatic;./manage.py migrate rb; ./manage.py migrate chronos; ./manage.py migrate analytics;'
