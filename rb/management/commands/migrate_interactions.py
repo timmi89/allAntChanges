d76d8fa93e97014d6bbc351824a92109e9e3bc5e
Michael Shaw <michaelshaw23@gmail.com>
2015-03-04 16:16:28 -0500

    tweaking migration logging for dry run

--- rb/management/commands/migrate_interactions.py
+++ rb/management/commands/migrate_interactions.py
@@ -16,18 +16,22 @@ class Command(BaseCommand):
         old_group = Group.objects.get(id = args[0])
         new_group = Group.objects.get(id = args[1])
         old_pages = Page.objects.filter(site__group = old_group)
-        op_paginator = Paginator(old_pages, 10)
+        op_paginator = Paginator(old_pages, 100)
+        logger.info('Page Range: ' + str(op_paginator.page_range))
         for x in op_paginator.page_range:
+            logger.info('STARTING PAGE: ' + str(x))
             for p in op_paginator.page(x).object_list:
-                logger.info('migrating: ' + p.canonical_url)
+                
                 try:
                     new_url = p.canonical_url.replace('http://' + old_group.short_name, '')
-                    new_page = Page.objects.filter(canonical_url = 'http://www.' + new_group.short_name + new_url, site__group = new_group)
+                    logger.info('migrating: ' + p.canonical_url + ' to: ' + 'http://www.' + new_group.short_name + new_url)
+                    new_page = Page.objects.get(canonical_url = 'http://www.' + new_group.short_name + new_url, site__group = new_group)
                     interactions = Interaction.objects.filter(page = p, page__site__group = old_group)
                     #interactions.update(page = new_page)
-                    logger.info(len(interactions))
+                    #logger.info(len(interactions))
                 except Page.DoesNotExist, pdne:
                     logger.warn('Unable to migrate: ' + str(p.id) + ' ' + p.canonical_url)
                 except Exception, ex:
                     logger.warn(ex)
+            logger.info('FINISHED PAGE')
         
