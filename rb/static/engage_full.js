8b1ffafbd1a696b97ee34bd799bb21a44d6b0654
Porter Bayne <porter@readrboard.com>
2015-04-12 12:26:48 -0400

    fix

--- rb/static/engage_full.js
+++ rb/static/engage_full.js
@@ -4239,8 +4239,8 @@ function antenna($A){
 
                                 // does this URL have the origin on it?  or does it just begin with a relative path?
                                 if ( url.indexOf(window.location.origin) == -1 ) {
-                                    // dont do this if we're supposed to ignore the subdomain
-                                    if (ANT.group.ignore_subdomain == true && ANT.group.page_tld) {
+                                    // dont do this if we're supposed to ignore the subdomains
+                                    if (ANT.group.ignore_subdomain != true) {
                                         if ( url.substr(0,1) == "/" ) {
                                             url = window.location.origin + url;
                                         } else {
