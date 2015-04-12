f87c5b4ec5d00c967c2ecef175e81b12a1ae2225
Porter Bayne <porter@readrboard.com>
2015-04-12 11:55:51 -0400

    emergency fixes

--- rb/static/engage_full.js
+++ rb/static/engage_full.js
@@ -4887,7 +4887,6 @@ function antenna($A){
                 ANT.actions.hashCustomDisplayHashes();
             },
             UIClearState: function(e){
-                    e.preventDefault();
                 // if (!isTouchBrowser) {
                     //ANT.actions.UIClearState:
                     // clear any errant tooltips
@@ -10157,7 +10156,7 @@ function $AFunctions($A){
         css.push( ANT_staticUrl+"widget/css/ie"+parseInt( $A.browser.version, 10) +".css" );
     }
 
-    var widgetCSS = ( ANT_offline ) ? ANT_widgetCssStaticUrl+"widget/css/newwidget.css" : ANT_widgetCssStaticUrl+"widget/css/newwidget.min.css?rv16"
+    var widgetCSS = ( ANT_offline ) ? ANT_widgetCssStaticUrl+"widget/css/newwidget.css" : ANT_widgetCssStaticUrl+"widget/css/newwidget.min.css?rv17"
     css.push( widgetCSS );
     // css.push( ANT_scriptPaths.jqueryUI_CSS );
     css.push( ANT_staticUrl+"widget/css/jquery.jscrollpane.css" );
