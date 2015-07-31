a76bd13954bbb9bfbe2da8c86bb9b8b3e4a1060e
Porter Bayne <porter@readrboard.com>
2015-07-30 19:35:39 -0700

    in progress... framework, grunt changes, etc

--- rb/static/grunt/Gruntfile.js
+++ rb/static/grunt/Gruntfile.js
@@ -2,11 +2,13 @@
 module.exports = function(grunt) {
 
     var paths = {
-        // TODO: fill in these values
-        web_css_src: [ '../site/css/*.css' ],
-        web_css_dest: '../site/styles.min.css',
-        web_js_src: '../site/js/*.js',
-        web_js_dest: '../site/antenna-web.min.js'
+        web_css_src: [ '../site/css/site_sass_compiled.css' ],
+        web_css_dest: '../site/css/styles.min.css',
+        web_scss_src: [ '../site/sass/*.scss' ],
+        web_scss_watch_src: [ '../site/sass/*.scss','../site/sass/antenna_components/*.scss','../site/sass/materialize_components/*.scss' ],
+        web_scss_dest: '../site/css/site_sass_compiled.css',
+        web_js_src: [ '../widget/js/jquery.json-2.2.min.js', '../widget/js/jquery.cookie.js', '../site/js/ant_site_scripts.js', '../widget/js/ant_user.js'],
+        web_js_dest: '../site/js/antenna-web.min.js'
     };
 
   // Project configuration.
@@ -30,10 +32,18 @@ module.exports = function(grunt) {
             web_js: {
                 options: {
                     banner: '<%= banner %>',
-                    stripBanners: true
+                    stripBanners: true,
+                    mangle: false
                 },
                 src: ['<%= paths.web_js_src %>'],
                 dest: '<%= paths.web_js_dest %>'
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    '<%= paths.web_scss_dest %>' : '<%= paths.web_scss_src %>'
+                }
             }
         },
         watch: {
@@ -43,6 +53,10 @@ module.exports = function(grunt) {
             web_js: {
                 files: [ '<%= paths.web_js_src %>' ],
                 tasks: [ 'uglify:web_js' ]
+            },
+            scss: {
+                files: [ '<%= paths.web_scss_watch_src %>' ],
+                tasks: ['sass']
             },
             web_css: {
                 files: [ '<%= paths.web_css_src %>' ],
@@ -54,9 +68,10 @@ module.exports = function(grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task.
-    grunt.registerTask('default', [ 'uglify:web_js', 'cssmin:web_css' ]);
+    grunt.registerTask('default', [ 'uglify:web_js', 'sass', 'cssmin:web_css' ]);
 
 };
