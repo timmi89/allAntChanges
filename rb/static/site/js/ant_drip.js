8ac44be64e4987b15a83e4e4b45ef5345dfb414f
Porter Bayne <porter@readrboard.com>
2015-08-07 12:50:24 -0700

    capturing leads into drip

--- rb/static/site/js/ant_drip.js
+++ rb/static/site/js/ant_drip.js
@@ -1,43 +1,47 @@
 ;var ANTdrip = ANTdrip ? ANTdrip : {};
 
-// ANTdrip.subscribe = function() {
-//     _dcq.push(["identify", {
-//       email: "john@acme.com",
-//       first_name: "John",
-//       tags: ["Customer"]
-//     }]);    
-// };
-
 ANTdrip.init = function() {
-    console.log('init drip form');
     $('.drip-form').each(function() {
       $(this).submit(function () {
         var $form = $(this);
 
-        var drip_form_id = $form.find('[name="fields[drip_form_id]"]').val(),
+        // var drip_form_id = $form.find('[name="fields[drip_form_id]"]').val(),
+        var valid = true,
             email = $form.find('[name="fields[email]"]').val(),
             website_url = $form.find('[name="fields[website_url]"]').val(),
             form_id = $form.find('[name="fields[form_id]"]').val(),
+            goal_name = $form.find('[name="fields[goal_name]"]').val(),
             fields = {
                 email: email,
                 website_url: website_url,
                 form_id: form_id
             };
 
-        console.log(drip_form_id);
-        console.log(fields);
-
-        _dcq.push(["subscribe", { 
-            campaign_id: drip_form_id, 
-            fields: fields,
-            double_optin: false,
-            success: function() {
-                console.log('SUCCESS YO!!!');
-            },
-            error: function() {
-                console.log('ack there was a bug');
+        $form.find('[data-required]').each(function() {
+            if ( !$(this).val() ) {
+                valid = false;
             }
-        }]);
+        });
+
+        if (valid === true) {
+            _dcq.push(["identify", {
+              email: email,
+              prospect: true,
+              website_url: website_url,
+              form_id:form_id,
+              success: function() {
+                _dcq.push(["track", goal_name ]);
+                $form.find('.success').removeClass('hide').siblings('.form-message').addClass('hide');
+              },
+              error: function() {
+                $form.find('.error').removeClass('hide').siblings('.form-message').addClass('hide');
+              }
+            }]);
+        } else {
+            $form.find('.not-valid').removeClass('hide').siblings('.form-message').addClass('hide');
+        }
+
+
         return false;
       });
     });
