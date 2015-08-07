d3930c5f3f8d3d9260b7b52af0a2699cc8dc45f4
Porter Bayne <porter@readrboard.com>
2015-08-07 15:09:39 -0700

    core marketing event tracking in place

--- rb/static/site/js/ant_drip.js
+++ rb/static/site/js/ant_drip.js
@@ -1,6 +1,73 @@
 ;var ANTdrip = ANTdrip ? ANTdrip : {};
 
 ANTdrip.init = function() {
+    ANTdrip.initForms();
+    ANTdrip.initCustomEventTracking();
+};
+
+ANTdrip.initCustomEventTracking = function() {
+    var urlParams = getUrlParams();
+
+    // fire load event if they have a specified source
+    if ( urlParams.ant_source ) {
+        // _dcq.push(["track", 'Visited Website', { 'ant_source': urlParams.ant_source, 'ant_value':urlParams.ant_value } ]);
+        var userFields = {};
+
+        userFields['ant_source'] = urlParams.ant_source;
+        userFields['ant_source'] = urlParams.ant_source;
+        // userFields['user_id'] = SOMETHING IF WE NEED TO ID ANONYMOUS USERS
+        userFields[ urlParams.ant_source + '_id' ] = urlParams.ant_value;
+        userFields['tags'] = [ urlParams.ant_source ];
+
+        _dcq.push(["identify", userFields ]);
+    }
+
+    // setup some custom events
+    $('#tawkchat-minified-iframe-element').ready(function(){
+        // wait a moment then apply this event.  wasn't working without some sort of a pause.
+        setTimeout( function() {
+            $('#tawkchat-minified-iframe-element').contents().find('body').click(function(){
+                _dcq.push(["track", 'Opened Tawk Chat' ]);
+            });
+
+        }, 750);
+    });
+
+    document.addEventListener("antenna.reaction", function(){
+      var lastEvent = antenna.getLastEvent();
+      if (lastEvent.event == 'antenna.reaction') {
+        _dcq.push(["track", 'Reacted', { 'page_title':document.title } ]);
+      }
+    },false);
+
+    document.addEventListener("antenna.reactionview", function(){
+      var lastEvent = antenna.getLastEvent();
+      if (lastEvent.event == 'antenna.reactionview') {
+        _dcq.push(["track", 'Viewed Reactions', { 'page_title':document.title } ]);
+      }
+    },false);
+
+    function getUrlParams(optQueryString) {
+        //thanks: http://stackoverflow.com/a/2880929/1289255
+        //I haven't verfied that this is 100% perfect for every url case, but it's solid.
+        
+        var queryString = optQueryString || window.location.search;
+
+        var urlParams = {};
+        var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^&=]+)=?([^&]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+        q = queryString.substring(1);
+
+        while (e = r.exec(q))
+            urlParams[d(e[1])] = d(e[2]);
+
+        return urlParams;
+    }
+};
+    
+ANTdrip.initForms = function() {
     $('.drip-form').each(function() {
       $(this).submit(function () {
         var $form = $(this);
