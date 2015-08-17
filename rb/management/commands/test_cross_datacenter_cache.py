8f2cf232f40f6f8253fa5c0ca37e055605846d9f
Michael <michael@antenna.is>
2015-08-17 01:44:35 -0400

    pagination

--- rb/management/commands/test_cross_datacenter_cache.py
+++ rb/management/commands/test_cross_datacenter_cache.py
@@ -15,24 +15,23 @@ class Command(BaseCommand):
 
         group  = Group.objects.get(id=group_id)
         print group
-        pages = Page.objects.filter(site__group = group)[0:100]
+        pages = Page.objects.filter(site__group = group)
         print len(pages)
+        page_paginator = Paginator(pages, 100)
+        for pr in page_paginator.page_range:
+            paginated_pages = page_paginator.page(pr)
+            for page in paginated_pages:
+                #update_page_cache.delay(page.id)
+                refresh_url = '/api/cache/page/refresh/'+ str(page.id)
+                print refresh_url
 
-        for page in pages:
-            print page.id
-            #update_page_cache.delay(page.id)
+                try:
+                    #hcon = httplib.HTTPConnection(host=settings.OTHER_DATACENTER)
+                    hcon = httplib.HTTPConnection(host="gce.antenna.is")
+                    hcon.request('GET', refresh_url)
+                    resp = hcon.getresponse()
+                    lines = resp.read()
+                    hcon.close()
+                except Exception, e:
+                    logger.info("Other datacenter refresh: " + str(e))
 
-            
-            refresh_url = '/api/cache/page/refresh/'+ str(page.id)
-            print refresh_url
-
-            try:
-                #hcon = httplib.HTTPConnection(host=settings.OTHER_DATACENTER)
-                hcon = httplib.HTTPConnection(host="gce.antenna.is")
-                hcon.request('GET', refresh_url)
-                resp = hcon.getresponse()
-                lines = resp.read()
-                hcon.close()
-            except Exception, e:
-                logger.info("Other datacenter refresh: " + str(e))
-            
