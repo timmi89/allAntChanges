3a56506abecdd103d8ecd3491261141e8d2bfda4
Michael Shaw <michaelshaw23@gmail.com>
2015-09-01 21:09:35 -0400

    more hosts... commit to switch

--- gce_deploytasks.sh
+++ gce_deploytasks.sh
@@ -1,4 +1,4 @@
-ARRAY="array-1 array-2 array-4 array-5 array-6 array-7 array-8 array-9 array-10 array-11 array-12 array-13 array-14"
+ARRAY="array-1 array-2 array-4 array-5 array-6 array-7 array-8 array-9 array-10 array-11 array-12 array-13 array-14 array-15 array-16"
 UTIL="utility-1"
 DBS="db-proto-1 db-proto-2"
 EVENTS="events2 events3 events4 events5 events6 events7 events8 events9"
