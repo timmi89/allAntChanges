5016555e3bce6759d126cac375e04f82a18db90f
Michael Shaw <michaelshaw23@gmail.com>
2015-09-29 20:33:23 -0400

    small tweaks for page info... slows down... check TODOs

--- forecast/reporting/utils.py
+++ forecast/reporting/utils.py
@@ -32,32 +32,50 @@ def aggregate_reports(group_reports, depth):
             daily['top_reaction_views_count'] = 0 
             for page_id in gr.sorted_pages:
                 if not agg_dict['pages'].has_key(page_id):
-                    agg_dict['pages'][page_id] = 0
-                agg_dict['pages'][page_id]      += gr.count_map[str(page_id)+'_score']
+                    agg_dict['pages'][page_id] = {}
+                    agg_dict['pages'][page_id]['score'] = 0
+                    page = Page.objects.get(id=page_id)
+                    agg_dict['pages'][page_id]['url'] = page.url
+                    agg_dict['pages'][page_id]['title'] = page.title
+                    agg_dict['pages'][page_id]['views'] = 0
+                    agg_dict['pages'][page_id]['reaction_views'] = 0
+                    agg_dict['pages'][page_id]['reactions'] = 0
+                    
+                agg_dict['pages'][page_id]['score']             += gr.count_map[str(page_id)+'_score']
+                agg_dict['pages'][page_id]['views']             += int(gr.count_map[str(page_id)+'_pageviews'])
+                agg_dict['pages'][page_id]['reaction_views']    += int(gr.count_map[str(page_id)+'_reaction_views'])
+                agg_dict['pages'][page_id]['reactions']         += int(gr.count_map[str(page_id)+'_reactions'])
+                #TODO these will need to be separate queries.
                 daily['top_views_count']        += int(gr.count_map[str(page_id)+'_pageviews'])
                 daily['top_reactions_count']    += int(gr.count_map[str(page_id)+'_reactions'])
                 daily['top_reaction_views_count']    += int(gr.count_map[str(page_id)+'_reaction_views'])
+                
             for (cid,pid) in gr.content_page.items():
                 if agg_dict['content'].has_key(cid):
-                    agg_dict['content'][cid]['score'] += agg_dict['pages'][page_id]
+                    agg_dict['content'][cid]['score'] += agg_dict['pages'][pid]['score']
                 else:
                     agg_dict['content'][cid] = {}
-                    agg_dict['content'][cid]['score']   = agg_dict['pages'][pid]
+                    agg_dict['content'][cid]['score']   = agg_dict['pages'][pid]['score']
                     agg_dict['content'][cid]['type']    = gr.pop_content_type[cid]
                     agg_dict['content'][cid]['body']    = gr.pop_content[cid]
+                    #TODO:  do db lookup after list is trimmed
                     try:
                         agg_dict['content'][cid]['interaction_id']    = Interaction.objects.filter(content__id = cid)[0].id
                     except Interaction.DoesNotExist, idne:
                         logger.warn('No Interaction for Content in Utils Reporting')
-                    
-                    
+                          
             for (rid,pid) in gr.reaction_page.items():
                 if agg_dict['reactions'].has_key(rid):
                     agg_dict['reactions'][rid]['score'] += agg_dict['pages'][pid]
                 else:
                     agg_dict['reactions'][rid] = {}
                     agg_dict['reactions'][rid]['score'] = agg_dict['pages'][pid]
-                    #TODO INTERACTION INFORMATION FOR GENERATING LINK?
+                    #TODO:  do db lookup after list is trimmed
+                    try:
+                        interaction = Interaction.objects.get(id=rid)
+                        agg_dict['reactions'][rid]['body'] = interaction.interaction_node.body
+                    except Interaction.DoesNotExist, idne:
+                        logger.warn(idne)
                     
             daily['uniques'] = gr.count_map['uniques']
             daily['engagement'] = gr.count_map['engagement']
@@ -72,8 +90,11 @@ def aggregate_reports(group_reports, depth):
             agg_dict['sorted_reactions'].sort(key = lambda entry : entry[1]['score'])
             agg_dict['sorted_reactions'].reverse()
         
-        
-        
+        agg_dict.pop('content')
+        agg_dict.pop('reactions')
+        agg_dict['sorted_content'] = agg_dict['sorted_content'][0:20]
+        agg_dict['sorted_reactions'] = agg_dict['sorted_reactions'][0:20]
+        #TODO:  do db lookups HERE on trimmed lists
         
         return json.dumps(agg_dict, cls=DatetimeEncoder)
     else:
