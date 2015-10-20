051d4d750f0e73a3de1f9ef2796ac49b83d18177
broadcaster <broadcaster@utility-1.c.antenna-array.internal>
2015-10-20 21:07:58 +0000

    array-24

--- gce_deploytasks.sh
+++ gce_deploytasks.sh
@@ -1,4 +1,4 @@
-ARRAY="array-1 array-2 array-4 array-5 array-6 array-7 array-8 array-9 array-10 array-11 array-12 array-13 array-14 array-15 array-16 array-17 array-18 array-19 array-20 array-21 array-22 array-23"
+ARRAY="array-1 array-2 array-4 array-5 array-6 array-7 array-8 array-9 array-10 array-11 array-12 array-13 array-14 array-15 array-16 array-17 array-18 array-19 array-20 array-21 array-22 array-23 array-24"
 UTIL="utility-1"
 DBS="db-proto-1 db-proto-2 db-proto-3"
 EVENTS="events2 events3 events4 events5 events6 events7 events8 events9"
