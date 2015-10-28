ee1765582aa2a944a04a06d04c4543b02232a5d7
Ghazgkull <jared.burns@krumplr.com>
2015-10-28 13:38:14 -0700

    refactor our on/off callback support (tagging callbacks with a uid) into a separate module

---
+++ rb/static/widget-new/src/js/utils/callback-support.js
@@ -0,0 +1,57 @@
+
+var antuid = 0; // "globally" unique ID that we use to tag callback functions for later retrieval. (This is how "off" works.)
+
+function createCallbacks() {
+
+    var callbacks = {};
+
+    function addCallback(callback) {
+        if (callback.antuid === undefined) {
+            callback.antuid = antuid++;
+        }
+        callbacks[callback.antuid] = callback;
+    }
+
+    function removeCallback(callback) {
+        if (callback.antuid !== undefined) {
+            delete callbacks[callback.antuid];
+        }
+    }
+
+    function getCallbacks() {
+        var allCallbacks = [];
+        for (var key in callbacks) {
+            if (callbacks.hasOwnProperty(key)) {
+                allCallbacks.push(callbacks[key]);
+            }
+        }
+        return allCallbacks;
+    }
+
+    // Convenience function that invokes all callbacks with no parameters. Any callbacks that need params can be called
+    // by clients using getCallbacks()
+    function invokeAll() {
+        for (var key in callbacks) {
+            if (callbacks.hasOwnProperty(key)) {
+                callbacks[key]();
+            }
+        }
+    }
+
+    function isEmpty() {
+        return Object.getOwnPropertyNames(callbacks).length === 0;
+    }
+
+    return {
+        add: addCallback,
+        remove: removeCallback,
+        get: getCallbacks,
+        isEmpty: isEmpty,
+        invokeAll: invokeAll
+    }
+}
+
+//noinspection JSUnresolvedVariable
+module.exports = {
+    create: createCallbacks
+};
