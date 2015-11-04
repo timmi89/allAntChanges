e33a04b539ec9395d3325e0eda70079c11f6ac6e
broadcaster <broadcaster@utility-1.c.antenna-array.internal>
2015-11-04 17:15:49 +0000

    switch dir so path relative manage call works

--- restart_celery.sh
+++ restart_celery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /home/broadcaster/antenna/
 ps aux | grep celeryd | awk '{print $2}' | xargs kill -9; ps aux | grep celerybeat | awk '{print $2}' | xargs kill -9
 echo 'Killed celeryd and celerybeat'
 nohup ./manage.py celerybeat --loglevel=info &
