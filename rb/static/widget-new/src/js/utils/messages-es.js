465732d694c744007128d447e856e549fa9bfd55
Ghazgkull <jared.burns@krumplr.com>
2016-01-19 13:54:08 -0800

    Spanish translation for back button

--- rb/static/widget-new/src/js/utils/messages-es.js
+++ rb/static/widget-new/src/js/utils/messages-es.js
@@ -9,7 +9,7 @@ module.exports = {
     'reactions-widget_title_signin': 'Sign in Required', // TODO: need a translation
     'reactions-widget_title_blocked': 'Blocked Reaction', // TODO: need a translation
 
-    'reactions-page_no_reactions': 'No reacciones!', // TODO: need a translation of "No reactions yet!"
+    'reactions-page_no_reactions': '¡No reacciones!', // TODO: need a translation of "No reactions yet!"
     'reactions-page_think': '¿Qué piensas?',
 
     'media-indicator_think': '¿Qué piensas?',
@@ -17,11 +17,11 @@ module.exports = {
     'popup-widget_think': '¿Qué piensas?',
 
     'defaults-page_add': '+ Añade lo tuyo',
-    'defaults-page_ok': 'ok', // TODO: is this right? 'acceptar'?
+    'defaults-page_ok': 'ok',
 
     'confirmation-page_share': 'Comparte tu reacción:',
 
-    'comments-page_back': 'Cerrar', // TODO: need a translation for "Back"
+    'comments-page_back': 'Volver',
     'comments-page_header': '({0}) Comentas:',
 
     'comment-area_add': 'Comenta',
@@ -32,16 +32,16 @@ module.exports = {
     'locations-page_pagelevel': 'A esta página', // TODO: need a translation of "To this whole page"
     'locations-page_count_one': '<span class="antenna-location-count">1</span><br>reacción',
     'locations-page_count_many': '<span class="antenna-location-count">{0}</span><br>reacciones',
-    'locations-page_back': 'Cerrar', // TODO: need a translation for "Back"
+    'locations-page_back': 'Volver',
 
     'call-to-action-label_responses': 'Respuestas', // TODO: need a translation of "Responses"
     'call-to-action-label_responses_one': '1 Respuesta', // TODO
     'call-to-action-label_responses_many': '{0} Respuestas', // TODO
 
-    'login-page_back': 'Cerrar', // TODO: need a translation for "Back",
+    'login-page_back': 'Volver',
 
-    'blocked-page_back': 'Cerrar', // TODO: need a translation for "Back"
+    'blocked-page_back': 'Volver',
 
-    'mobile-helper_prompt': '¡Toca un párrafo para opinar!', // TODO: need translation
-    'mobile-helper_close': 'Cerrar'
+    'mobile-helper_prompt': '¡Toca un párrafo para opinar!',
+    'mobile-helper_close': 'Volver'
 };
