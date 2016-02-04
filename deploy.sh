2204ea137ccd008ffd193edf97f53d3d111e9852
Brian Finney <bri@nfinney.com>
2016-02-03 19:22:19 -0800

    cleanup old deploy code

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Setting up ssh-agent."
-ssh-add
-
-echo "Configuring environment."
-. deploytasks.sh
-
-foreach_exec_broadcaster "$ARRAY" "Basic update" "$BASIC"
-foreach_exec_root "$ARRAY" "Restart antenna" "$SUPER"
--- restart_celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/broadcaster/antenna/
-ps aux | grep celeryd | awk '{print $2}' | xargs kill -9; ps aux | grep celerybeat | awk '{print $2}' | xargs kill -9
-echo 'Killed celeryd and celerybeat'
-nohup ./manage.py celerybeat --loglevel=info &
-echo 'Restarting celerybeat'
-nohup ./manage.py celeryd --loglevel=info --concurrency=10 &
-#nohup ./manage.py celeryd --loglevel=info &
-echo 'Restarting celeryd'
