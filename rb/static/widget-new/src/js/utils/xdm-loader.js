062b1908ef132ecea68e442882773467097dadd7
Ghazgkull <jared.burns@krumplr.com>
2016-03-24 17:30:52 -0700

    encodeURIComponent in xdm loader

--- rb/static/widget-new/src/js/utils/xdm-loader.js
+++ rb/static/widget-new/src/js/utils/xdm-loader.js
@@ -7,8 +7,8 @@ var XDM_ORIGIN = AppMode.offline ? URLConstants.DEVELOPMENT : URLConstants.PRODU
 
 function createXDMframe(groupId) {
     var iframeUrl = XDM_ORIGIN + '/static/widget-new/xdm-new.html',
-    parentUrl = encodeURI(window.location.href),
-    parentHost = encodeURI(window.location.protocol + '//' + window.location.host),
+    parentUrl = encodeURIComponent(window.location.href),
+    parentHost = encodeURIComponent(window.location.protocol + '//' + window.location.host),
     $xdmIframe = $('<iframe id="ant-xdm-hidden" name="ant-xdm-hidden" src="' + iframeUrl + '?parentUrl=' + parentUrl + '&parentHost=' + parentHost + '&group_id='+groupId+'" width="1" height="1" style="position:absolute;top:-1000px;left:-1000px;" />');
     $(WidgetBucket.get()).append( $xdmIframe );
 }
