3c4a47d2e55b005d94ba8d2abf2e3ac03cf23940
Ghazgkull <jared.burns@krumplr.com>
2016-04-07 10:15:23 -0700

    new script to simply monitor our task queue and restart celery if needed

---
+++ config/monitor_task_queue.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+
+def main():
+    proc = os.popen(
+        "/usr/sbin/rabbitmqctl list_queues -p antenna_broker name messages consumers | grep -v utility | grep -v celeryev | grep celery | awk '{print $2;}'"
+    )
+    queue_size = int(proc.readline().strip())
+    proc.close()
+
+    if queue_size > 50:
+        print "cloud_metrics restarting celery: queue_size=%d" % queue_size
+        os.putenv('PYTHONPATH', '/home/broadcaster')
+        res_proc = os.popen(
+            "su broadcaster --preserve-environment -c /home/broadcaster/antenna/restart_celery.sh"
+        )
+        res_proc.close()
+        curl_command = "curl -X POST --data-urlencode 'payload={{\"channel\": \"#system_alerts\", \"username\": \"webhookbot\", \"text\": \"Restarting Celery. (queue size: {0})\", \"icon_emoji\": \":rabbit:\"}}' https://hooks.slack.com/services/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".format(queue_size)
+        slack_proc = os.popen(
+            curl_command
+        )
+        slack_proc.close()
+
+
+if __name__ == "__main__":
+    main()
