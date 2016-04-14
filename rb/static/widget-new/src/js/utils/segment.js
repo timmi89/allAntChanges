aac57f1cd3a24f44d9f4ec3156de7ded091b7358
Ghazgkull <jared.burns@krumplr.com>
2016-04-14 16:23:07 -0700

    A/B test one-page widget on perezhilton and mobi.perezhilton

--- rb/static/widget-new/src/js/utils/segment.js
+++ rb/static/widget-new/src/js/utils/segment.js
@@ -1,6 +1,5 @@
 var UrlParams = require('./url-params');
 
-var segments = [ '250', '400', '700' ];
 var segment;
 
 function getSegment(groupSettings) {
@@ -11,47 +10,55 @@ function getSegment(groupSettings) {
 }
 
 function computeSegment(groupSettings) {
+    var segments = [ '2p', '1p' ];
+    if (groupSettings.groupId() === 3714) {
+        segments = [ '250', '400', '700' ];
+    }
     var segmentOverride = UrlParams.getUrlParam('antennaSegment');
     if (segmentOverride) {
         storeSegment(segmentOverride);
         return segmentOverride;
     }
     var segment = readSegment();
-    if (!segment && (groupSettings.groupId() === 3714 || groupSettings.groupId() === 2)) {
+    if (!segment && groupSettings.groupId() === 3714 || groupSettings.groupId() === 2 || groupSettings.groupId() === 2504 || groupSettings.groupId() === 2471) {
         segment = createSegment(groupSettings);
         segment = storeSegment(segment);
     }
     return segment;
-}
 
-function readSegment() {
-    // Returns the stored segment, but only if it is one of the current valid segments.
-    var segment = localStorage.getItem('ant_segment');
-    if (segment) {
-        for (var i = 0; i < segments.length; i++) {
-            if (segment === segments[i]) {
-                return segment; // Valid segment. Return.
+    function readSegment() {
+        // Returns the stored segment, but only if it is one of the current valid segments.
+        var segment = localStorage.getItem('ant_segment');
+        if (segment) {
+            for (var i = 0; i < segments.length; i++) {
+                if (segment === segments[i]) {
+                    return segment; // Valid segment. Return.
+                }
             }
         }
     }
-}
 
-function createSegment(groupSettings) {
-    return segments[Math.floor(Math.random() * segments.length)];
-}
-
-function storeSegment(segment) {
-    try {
-        localStorage.setItem('ant_segment', segment);
-    } catch(error) {
-        // Some browsers (mobile Safari) throw an exception when in private browsing mode.
-        // If this happens, fall back to a default value that will at least give us stable behavior.
-        segment = segments[0];
+    function createSegment(groupSettings) {
+        return segments[Math.floor(Math.random() * segments.length)];
     }
-    return segment;
+
+    function storeSegment(segment) {
+        try {
+            localStorage.setItem('ant_segment', segment);
+        } catch(error) {
+            // Some browsers (mobile Safari) throw an exception when in private browsing mode.
+            // If this happens, fall back to a default value that will at least give us stable behavior.
+            segment = segments[0];
+        }
+        return segment;
+    }
 }
 
+function isInOnePageSegment(groupSettings) {
+    return getSegment(groupSettings) === '1p';
+}
 
 module.exports = {
-    getSegment: getSegment
+    getSegment: getSegment,
+    isOnePage: isInOnePageSegment
 };
