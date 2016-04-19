654df3a3e5915627187dbceaf532e812d3cfbfb4
Brian Finney <bri@nfinney.com>
2016-04-19 12:27:35 -0700

    mysql load progress monitoring

--- antenna-array-db_to_gke.sh
+++ antenna-array-db_to_gke.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 apt-get update
-apt-get install -y mysql-client
+apt-get install -y mysql-client pv time
 
 date
 echo 'doing dump'
 mysqldump -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --add-drop-database --master-data=2 --single-transaction --databases readrboard > db_dump.sql
 date
 echo 'dump complete, loading'
-mysql -h $DATABASE_HOST_MASTER -uantenna-array -pr34drsl4v3 --ssl --ssl-key $DATABASE_SSL_MASTER_CLIENT_KEY --ssl-cert $DATABASE_SSL_MASTER_CLIENT_CERT --ssl-ca $DATABASE_SSL_MASTER_SERVER_CA < db_dump.sql
+pv db_dump.sql | mysql -h $DATABASE_HOST_MASTER -uantenna-array -pr34drsl4v3 --ssl --ssl-key $DATABASE_SSL_MASTER_CLIENT_KEY --ssl-cert $DATABASE_SSL_MASTER_CLIENT_CERT --ssl-ca $DATABASE_SSL_MASTER_SERVER_CA
 date
 echo 'done'
