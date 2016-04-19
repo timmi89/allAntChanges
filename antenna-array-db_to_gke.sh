843b936f31fa206eca1d62d95521209e05c21bb3
Brian Finney <bri@nfinney.com>
2016-04-18 20:43:42 -0700

    include binlog info in antenna-array-db script

--- antenna-array-db_to_gke.sh
+++ antenna-array-db_to_gke.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y mysql-client
 
-mysqldump -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --add-drop-database --databases readrboard > db_dump.sql
+mysqldump -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --add-drop-database --master-data=2 --single-transaction --databases readrboard > db_dump.sql
 mysql -h $DATABASE_HOST_MASTER -uantenna-array -pr34drsl4v3 --ssl --ssl-key $DATABASE_SSL_MASTER_CLIENT_KEY --ssl-cert $DATABASE_SSL_MASTER_CLIENT_CERT --ssl-ca $DATABASE_SSL_MASTER_SERVER_CA < db_dump.sql
