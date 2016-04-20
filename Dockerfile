54a287e82f965b9a85f7ba0b9d03e1c03f1c33c7
Brian Finney <bri@nfinney.com>
2016-04-20 10:40:35 -0700

    update mysql libs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM python:2.7.11
 
+# Update MySQL libs
+ADD docker/apt/mysql.list /etc/apt/sources.list.d/mysql.list
+RUN apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5 && \
+    apt-get update && apt-get install -y lsb-release && \
+    apt-get upgrade -y mysql-client mysql-connector-python-cext && \
+    apt-get autoremove -y
+
 ENV PYTHONUNBUFFERED 1
 ENV DJANGO_SETTINGS_MODULE antenna.settings
 
--- antenna-array-db_to_gke.sh
+++ antenna-array-db_to_gke.sh
@@ -5,9 +5,11 @@ apt-get install -y mysql-client pv time
 
 date
 echo 'doing dump'
-mysqldump -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --add-drop-database --master-data=2 --single-transaction --databases readrboard > db_dump.sql
+mysqldump -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --add-drop-database --master-data=2 --single-transaction --databases readrboard | pv > db_dump.sql
 date
 echo 'dump complete, loading'
 pv db_dump.sql | mysql -h $DATABASE_HOST_MASTER -uantenna-array -pr34drsl4v3 --ssl --ssl-key $DATABASE_SSL_MASTER_CLIENT_KEY --ssl-cert $DATABASE_SSL_MASTER_CLIENT_CERT --ssl-ca $DATABASE_SSL_MASTER_SERVER_CA
 date
 echo 'done'
+echo
+echo 'mysqlbinlog -R -h 10.240.245.89 -uantenna-array -pr34drsl4v3 --start-position=$position --base64-output=never --stop-never $binlog | grep -v "SET @@session.pseudo_thread_id" | pv | mysql -h $DATABASE_HOST_MASTER -uantenna-array -pr34drsl4v3 --ssl --ssl-key $DATABASE_SSL_MASTER_CLIENT_KEY --ssl-cert $DATABASE_SSL_MASTER_CLIENT_CERT --ssl-ca $DATABASE_SSL_MASTER_SERVER_CA'
