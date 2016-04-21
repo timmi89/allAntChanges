f511dc54099aa52e9dc7bc03326b9767a3a2114e
Ghazgkull <jared.burns@krumplr.com>
2016-04-20 17:04:18 -0700

    revert DB settings for current deployment (continue using our custom mysql boxes)

--- settings.py
+++ settings.py
@@ -178,15 +178,10 @@ else:
             'NAME':     'readrboard',
             'USER':     'antenna-array',
             'PASSWORD': 'r34drsl4v3',
-            'HOST':     '104.197.217.109',
+            'HOST':     '10.240.99.122',
             'PORT':     '3306',
             'CONN_MAX_AGE':  60,
             'OPTIONS': {
-                'ssl': {
-                    'key': './db/master.key',
-                    'cert': './db/master.cert',
-                    'ca': './db/master.ca'
-                },
                 'charset': 'utf8',
                 'init_command': '''
                     SET
@@ -200,15 +195,27 @@ else:
             'NAME':     'readrboard',
             'USER':     'antenna-array',
             'PASSWORD': 'r34drsl4v3',
-            'HOST':     '104.197.217.109',
+            'HOST':     '10.240.245.89',
             'PORT':     '3306',
             'CONN_MAX_AGE':  60,
             'OPTIONS': {
-                'ssl': {
-                    'key': './db/replica.key',
-                    'cert': './db/replica.cert',
-                    'ca': './db/replica.ca'
-                },
+                'charset': 'utf8',
+                'init_command': '''
+                    SET
+                    default_storage_engine=INNODB,
+                    character_set_connection=utf8
+                '''
+            }
+        },
+        'readonly2': {
+            'ENGINE':   'django.db.backends.mysql',
+            'NAME':     'readrboard',
+            'USER':     'antenna-array',
+            'PASSWORD': 'r34drsl4v3',
+            'HOST':     '10.240.4.119',
+            'PORT':     '3306',
+            'CONN_MAX_AGE':  60,
+            'OPTIONS': {
                 'charset': 'utf8',
                 'init_command': '''
                     SET
