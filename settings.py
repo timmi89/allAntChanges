5d0b941b352beafefe3ea6aa58675c8395cbebbb
Brian Finney <bri@nfinney.com>
2016-04-28 14:10:30 -0700

    reduce cache max age for collect static

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ AWS_STORAGE_BUCKET_NAME = "readrboard"
 AWS_CALLING_FORMAT = ""
 AWS_HEADERS = {
     'Expires': 'Thu, 15 Apr 2020 20:00:00 GMT',
-    'Cache-Control': 'public, max-age=25200',
+    'Cache-Control': 'public, max-age=900',
 }
 
 AWS_DEFAULT_ACL = 'public-read'
