5d442c130a7245d0193bb35b6640dd5b5fed3b1a
Brian Finney <bri@nfinney.com>
2016-05-10 13:01:24 -0700

    be less quite where an error is happening

--- docker/env/build.sh
+++ docker/env/build.sh
@@ -42,6 +42,6 @@ fi
 
 echo Building ${IMAGE}-${ENVIRONMENT}:$VERSION
 docker/env/template.sh docker/env/Dockerfile.template | \
-  docker build -qt ${IMAGE}-${ENVIRONMENT}:$VERSION -
+  docker build -t ${IMAGE}-${ENVIRONMENT}:$VERSION -
 echo Pushing ${IMAGE}-${ENVIRONMENT}:$VERSION
 gcloud docker push ${IMAGE}-${ENVIRONMENT}:$VERSION | grep digest
